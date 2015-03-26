1dba816851bb35c2c24c6ed94821c0b8fa0c809b
poiuty <poiuty@lepus.su>
2015-03-27 00:04:21 +0300

    api change user passwd

--- web/private/func.php
+++ web/private/func.php
@@ -1,17 +1,44 @@
 <?
-
-function status_user($id, $status){
-	global $db;
-	if($status != 0 && $status != 1) return "wrong status $status";
+function passwd_user(){
+	global $db; $uid = $_GET['uid']; $hash = $_GET['hash'];
+	
+	if(!isset($uid)) return 'empty uid';
+	if(!isset($hash)) return 'empty hash';
+	if(!is_numeric($uid)) return 'not int uid';
+	
 	$query = $db->prepare("SELECT * FROM `users` WHERE `id` =:id");
-	$query->bindParam(':id', $id, PDO::PARAM_STR);
+	$query->bindParam(':id', $uid, PDO::PARAM_STR);
+	$query->execute();
+	if($query->rowCount() != 1) return 'error';
+	
+	$query = $db->prepare("UPDATE `users` SET `passwd` = :hash WHERE `id` =:id");
+	$query->bindParam(':id', $uid, PDO::PARAM_STR);
+	$query->bindParam(':hash', $hash, PDO::PARAM_STR);
+	$query->execute();
+	return 'OK';
+}
+
+function status_user(){
+	global $db; $uid = $_GET['uid']; $status = $_GET['status'];
+
+	if(!isset($uid)) return 'empty uid';
+	if(!isset($status)) return 'empty status';
+	if(!is_numeric($uid)) return 'not int uid';
+	if(!is_numeric($status)) return 'not int status';
+	if($status != 0 && $status != 1) return "wrong status";
+	
+	$query = $db->prepare("SELECT * FROM `users` WHERE `id` =:id");
+	$query->bindParam(':id', $uid, PDO::PARAM_STR);
 	$query->execute();
 	if($query->rowCount() != 1) return 'error';
 	
 	$query = $db->prepare("UPDATE `users` SET `block` = :status WHERE `id` =:id");
-	$query->bindParam(':id', $id, PDO::PARAM_STR);
+	$query->bindParam(':id', $uid, PDO::PARAM_STR);
 	$query->bindParam(':status', $status, PDO::PARAM_STR);
 	$query->execute();
+	
+	// if status=1 -> get_list_user_servers and shutdown it.
+	// if status=0 -> get_list_user_servers and turnon it.
 	return 'OK';
 }
 
--- web/public/api.php
+++ web/public/api.php
@@ -3,16 +3,12 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/private/config.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/private/init/mysql.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/private/func.php');
 
-if($_POST['api_key'] != $conf['api_key']) die('wrong api key');
+//if($_POST['api_key'] != $conf['api_key']) die('wrong api key');
 
 switch($_GET['do']){
 	default: echo "error"; break;
 	case 'register': break;
-	case 'passwd': break;
-	case 'status':
-		if(empty((int)$_POST['uid'])) die('error');
-		if(empty((int)$_POST['status'])) die('error');
-		status_user((int)$_POST['uid']),(int)$_POST['status']);
-	break;
+	case 'passwd': echo passwd_user(); break;
+	case 'status': echo status_user(); break;
 	case 'delete': break;
 }
